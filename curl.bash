3224e6cb2bec868990e43bc58d3e3fa42aa8dff9
rhett.rademacher <rhett.rademacher@rhett.rademacher--MacBook-Pro---DJ7WKK4M19>
2025-03-06 15:22:38 -0800

    update

---
+++ curl.bash
@@ -0,0 +1,5 @@
+https://hooks.slack.com/services/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
+
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"webhook test"}' https://hooks.slack.com/services/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
+
